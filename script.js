7b5055cc105694493e1742f2de67e4bab989d132
Wescley Christian <108875067+wcbs2006@users.noreply.github.com>
2023-06-15 22:17:09 -0300

    Update 01

--- script.js
+++ script.js
@@ -27,3 +27,39 @@ fetch('https://api.github.com/users/wcbs2006')
         console.error('Ocorreu um erro:',error);
     })
 
+
+fetch('https://api.github.com/users/wcbs2006/followers')
+    .then(response => response.json())
+    .then(data => {
+        const followersGrid = document.querySelector('.followers-grid');
+        let numSeguidoresExibidos = 6;
+        const viewMoreButton = document.querySelector('.view-more-followers');
+
+        const renderizarSeguidores = (() => {
+            followersGrid.innerHTML = '';
+            data.slice(0, numSeguidoresExibidos).forEach(follower => {
+                const followerPhotoUrl = follower.avatar_url;
+                const followerName = follower.login;
+    
+                const followerHTML = `
+                    <div class="follower" id="">
+                        <img src="${followerPhotoUrl}" alt="" class="follower-photo" id="1">
+                        <p class="follower-name">${followerName}</p>
+                    </div>
+                `;
+    
+                followersGrid.innerHTML += followerHTML;
+            });
+        });
+
+        viewMoreButton.addEventListener('click', () => {
+            numSeguidoresExibidos += 3;
+            renderizarSeguidores();
+        });
+
+        renderizarSeguidores();
+    })
+    .catch(error => {
+        console.error("Erro:", error);
+    });
+
